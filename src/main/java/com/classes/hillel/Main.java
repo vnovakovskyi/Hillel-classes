e48d521e74b131233a4b2dea3763d03f57f14ff6
Vadym Novakovskyi <vadim.novakovskiy-external@sema4genomics.com>
2021-03-21 13:37:41 +0200

    Added new sout

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -3,5 +3,6 @@ package main.java.com.classes.hillel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from git!");
+        System.out.println("new message");
     }
 }
