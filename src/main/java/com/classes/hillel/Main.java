56667558cf7f4104c05e276a00bf5e7ab18a3667
Vadym Novakovskyi <vadim.novakovskiy-external@sema4genomics.com>
2021-03-21 14:56:54 +0200

    Created arrays; wrote array to the console

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -2,7 +2,26 @@ package main.java.com.classes.hillel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from git!");
-        System.out.println("new message");
+        String [] array = new String[2];
+        int [] array2 = {1,2,3};
+
+        array[0] = "Hello";
+        array[1] = "world";
+
+        System.out.println(array[array.length-1]);
+
+
+
+
+
+
+
+//        for (int i = 0; i<array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+//
+//        for (int i = 0; i<array2.length; i++) {
+//            System.out.println(array2[i]);
+//        }
     }
 }
