6c478d2cc42b7dfc438cda8b979ecf4786bcdc05
Vadym Novakovskyi <vadim.novakovskiy-external@sema4genomics.com>
2021-04-04 15:59:22 +0300

    Added entering text from console

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,36 +1,108 @@
 package main.java.com.classes.hillel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//        boolean c1 = true & false & true;
+//        boolean c2 = true && false && true;
+//        SOLID
+
 public class Main {
+    static String[] alphabet = {"a", "b", "c", "d",
+            "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r",
+            "s", "t", "u", "v", "w", "x", "y", "z"};
+    static BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
-        String[] array = new String[2];
-        int[] array2 = {1, 2, 3};
+        String[] fullAlphabet = completeAlphabet();
 
-        array[0] = "Hello";
-        array[1] = "world";
+        System.out.println("Press shift number, please!");
+        int number = getShiftFromConsole();
 
-        System.out.println(array[array.length - 1]);
+        System.out.println("Please enter text to encode:");
+        String enteredText = getEncodingTextFromConsole();
 
-        String[] alphabet = {"a", "b", "c", "d",
-                "e", "f", "g", "h", "i", "j", "k",
-                "l", "m", "n", "o", "p", "q", "r",
-                "s", "t", "u", "v", "w", "x", "y", "z"};
+        String encryptedText = encrypt(enteredText,
+                fullAlphabet, number);
 
-        String encryptedWord = encrypt("Hello",
-                alphabet, 3);
-        System.out.println(encryptedWord);
+        System.out.println(encryptedText);
+    }
+
+    private static int getShiftFromConsole() {
+        String text = "";
+        try {
+            text = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (text == null || text.isEmpty()) {
+            text = "0";
+        }
+
+        int number = 0;
+        try {
+            number = Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return number;
+    }
+
+    private static String getEncodingTextFromConsole() {
+        String encodingText = "";
+        try {
+            encodingText = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return encodingText;
+    }
+
+    private static String[] completeAlphabet() {
+        String [] alphabetWithCapitalLetters
+                = fillCapitalLetters();
+
+        String [] alphabetWithSpecialSymbols
+                = fillSpecialSymbols(alphabetWithCapitalLetters);
+
+        return alphabetWithSpecialSymbols;
+    }
+
+    private static String[] fillSpecialSymbols(String [] newAlphabet) {
+        //Some actions
+        return newAlphabet;
+    }
+
+    private static String[] fillCapitalLetters() {
+        String [] newAlphabet = new String[alphabet.length * 2];
+
+        for (int i = 0; i < alphabet.length; ++i) {
+            newAlphabet[i] = alphabet[i];
+        }
+
+        for (int i = alphabet.length; i < newAlphabet.length; ++i) {
+            newAlphabet[i] = alphabet[i - alphabet.length].toUpperCase();
+        }
+        return newAlphabet;
     }
 
     private static String encrypt(String word,
-                                String[] alphabet,
-                                int shift){
+                                  String[] alphabet,
+                                  int shift) {
         String encrypted = "";
-        for (int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             int index = findIndex(alphabet, word.charAt(i));
-            if (index+shift > alphabet.length-1){
-                int innerIndex = (index+shift) - alphabet.length;
-                index = innerIndex-1;
+            if (index + shift > alphabet.length - 1) {
+                int innerIndex = (index + shift) - alphabet.length;
+                index = innerIndex - 1;
             }
-            encrypted = encrypted.concat(alphabet[index+shift]);
+            encrypted = encrypted.concat(alphabet[index + shift]);
         }
         return encrypted;
     }
@@ -38,7 +110,7 @@ public class Main {
     private static int findIndex(String[] alphabet,
                                  char letter) {
         int index = 0;
-        for (int i = 0; i<alphabet.length; i++){
+        for (int i = 0; i < alphabet.length; i++) {
             String ch = alphabet[i];
             if (ch.equals(String.valueOf(letter))) {
                 return i;
